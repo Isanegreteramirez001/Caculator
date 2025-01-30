092203a24eb4ad27ffcfe93e21f2f58c3ed626dc
Isanegreteramirez001 <isaiah.negrete_ramirez24@student.cart.org>
2025-01-30 14:21:41 -0800

    1

---
+++ Caculator.js
@@ -0,0 +1,33 @@
+function Caculator(_width=4,_height=4) {  
+} 
+let number = 0 ;
+let condition = M;
+let condition1 = D;
+let condition2 = A;
+let condition3 = S;
+let condition4 = P;
+let condition5 = E;
+  console.log(number);
+  number = number ;
+  if (condition=X===0){
+   } elseif (condition1=D===0);
+   {
+} elseif (condition2=number1+0===1)
+let number1 = 1;
+  console.log(number1);
+  number1 = number1;{
+} elseif (condition3=number1-0===-1)
+let parent = P;
+console.log(parent);
+parent = (P);{
+}elseif (condition4=parent===(0))
+{
+}elseif (condition5=E===any)
+{
+`
+ 1 2 3 M
+ 4 5 6 D
+ 7 8 9 A
+ P 0 E S
+`
+}
